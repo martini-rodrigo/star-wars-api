bd41aae0a46ec4add5fc68bb0ae13c0b94d64c99
rodrigo.martini@vlzm.com.br <rodrigo.martini@vlzm.com.br>
2025-06-14 08:11:34 -0300

    add log

--- src/main/java/br/com/starwars/core/repository/MovieRepository.java
+++ src/main/java/br/com/starwars/core/repository/MovieRepository.java
@@ -1,22 +1,26 @@
 package br.com.starwars.core.repository;
 
 import br.com.starwars.core.domain.entity.Movie;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Repository
 public class MovieRepository {
 
     private final List<Movie> movies = new ArrayList<>();
 
     public List<Movie> findAll() {
+        log.info("Finding all movies. Total movies: {}", movies.size());
         return new ArrayList<>(movies);
     }
 
     public Optional<Movie> findByTitle(String title) {
+        log.info("Searching for movie with title: {}", title);
         return movies.stream()
                 .filter(movie -> movie.getTitle().equalsIgnoreCase(title))
                 .findFirst();
@@ -24,6 +28,7 @@ public class MovieRepository {
 
     public void save(Movie movie) {
         if (!movies.contains(movie)) {
+            log.info("Adding movie: {}", movie.getTitle());
             movies.add(movie);
         }
     }

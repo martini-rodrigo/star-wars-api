b18b8e2f0d39b69f6357dd35591bc47f06e4c42e
rodrigo.martini@vlzm.com.br <rodrigo.martini@vlzm.com.br>
2025-06-14 14:50:08 -0300

    add swagger config

--- src/main/java/br/com/starwars/adapters/controller/MovieController.java
+++ src/main/java/br/com/starwars/adapters/controller/MovieController.java
@@ -1,10 +1,11 @@
 package br.com.starwars.adapters.controller;
 
 import br.com.starwars.core.movie.domain.Movie;
+import br.com.starwars.core.movie.dto.MovieUpdateDTO;
 import br.com.starwars.core.movie.usecase.GetMovieByTitleUseCase;
 import br.com.starwars.core.movie.usecase.ListMoviesUseCase;
-import br.com.starwars.core.movie.dto.MovieUpdateDTO;
 import br.com.starwars.core.movie.usecase.UpdateMovieUseCase;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -23,18 +24,21 @@ public class MovieController {
     private final UpdateMovieUseCase updateMovieUseCase;
 
     @GetMapping
+    @Operation(summary = "Retrieve all movies", description = "Returns a list of all movies.")
     public ResponseEntity<List<Movie>> getAll() {
         List<Movie> movies = listMoviesUseCase.getAll();
         return ResponseEntity.ok(movies);
     }
 
     @GetMapping("/{title}")
+    @Operation(summary = "Get movie details", description = "Retrieves detailed information about a specific movie by its title.")
     public ResponseEntity<Movie> getDetails(@PathVariable String title) {
         Movie movie = getMovieByTitleUseCase.getDetails(title);
         return ResponseEntity.ok(movie);
     }
 
     @PutMapping("/{title}/description")
+    @Operation(summary = "Update movie description", description = "Updates the description of a specific movie by its title.")
     public ResponseEntity<Void> updateDescription(
             @PathVariable String title,
             @RequestBody @Valid MovieUpdateDTO movieUpdateDTO) {
